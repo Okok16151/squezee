f875a179fb4f0ddcbd5b130b3c468d81161bba84
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-03-11 14:31:46 +0800

    Add files via upload

---
+++ assets/preload/data/plugwalk/script.lua
@@ -0,0 +1,11 @@
+local allowCountdown = false;
+local playDialogue = false;
+function onStartCountdown()
+	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		startVideo('week1song1');
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
