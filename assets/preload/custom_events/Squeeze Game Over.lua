037acc7538e10636d052f8f21f10411b9b1b12af
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-03-11 14:23:52 +0800

    Add files via upload

---
+++ assets/preload/custom_events/Squeeze Game Over.lua
@@ -0,0 +1,4 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'squeeze-playable'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_squeeze_loss_sfx'); --put in mods/sounds/
+end
